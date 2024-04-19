aabdddcc1751e430b44e668a9109691d4cf5ebde
danimendez12 <danmendez@estudiantec.cr>
2024-04-18 19:27:07 -0600

    Prueba1

---
+++ src/main/java/itcr/royectobarberia1/RoyectoBarberia1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itcr.royectobarberia1;
+
+/**
+ *
+ * @author danielmendez
+ */
+public class RoyectoBarberia1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
